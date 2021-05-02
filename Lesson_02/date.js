26a9b0b8ba4542e213631404d1409ae14d7bb65e
A1Ronin <tracyrtyler@gmail.com>
2021-05-01 17:40:39 -0700

    u

--- Lesson_02/date.js
+++ Lesson_02/date.js
@@ -1,3 +1,6 @@
-var dt = new Date(document.lastModified);
-document.getElementById('date-time').innerHTML = dt;
+document.querySelector('#current-date').textContent = document.lastModified;
 
+var options = {
+    year: 'numeric'
+};
+document.querySelector('#current-year').textContent = new Date().toLocaleDateString('en-US', options);
